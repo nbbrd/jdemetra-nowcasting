06cfa2f55225f39bac138b35a06132695dc608a5
Philippe Charles <philippe.charles@nbb.be>
2014-03-05 13:07:56 +0000

    Code clean up.
    git-svn-id: http://srvdqrd2:8008/svn/jdemetra_seer/trunk@49 6a911cac-6de7-584c-b8a8-48824e541a73

--- jtss2/src/main/java/ec/tss/Dfm/Rotation.java
+++ jtss2/src/main/java/ec/tss/Dfm/Rotation.java
@@ -1,51 +1,60 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 National Bank of Belgium
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and 
+ * limitations under the Licence.
  */
 package ec.tss.Dfm;
 
 import ec.tstoolkit.maths.matrices.Matrix;
 import ec.tstoolkit.maths.matrices.MatrixException;
+import ec.tstoolkit.utilities.Arrays2;
 
 /**
- * Builds a Rotation object that contains:
- * - The rotation matrix R itself, which is the product of q(q-1)/2 rotation 
- *   matrices representing all possible pairwise angular rotations.
- * - An array of angles (as many angles as needed to rotate all possible pairs 
- *   of dimensions of an hypothetical vector of size q). 
+ * Builds a Rotation object that contains: - The rotation matrix R itself, which
+ * is the product of q(q-1)/2 rotation matrices representing all possible
+ * pairwise angular rotations. - An array of angles (as many angles as needed to
+ * rotate all possible pairs of dimensions of an hypothetical vector of size q).
  * - The dimension q of an hypothetical vector to which the rotation is applied
- *   q = (int) (0.5 * (1 + Math.sqrt(1 + 8 * angles.length)));
- *   
- *  Notes:
- * - Because the array of angles univocally determines the rotation matrix,
- *   we define it as a final variable. 
- * - If R was declared as final, we would have to define it in the Rotation 
- *   constructor (as we have done for "angles"). However, at the current 
- *   development stage, we are not sure that the calculation of R as a function 
- *   of angles and multiplying them by a given vector is more efficient that 
- *   applying a rotation directly to a multidimensional axis.
- *   
+ * q = (int) (0.5 * (1 + Math.sqrt(1 + 8 * angles.length)));
+ *
+ * Notes: - Because the array of angles univocally determines the rotation
+ * matrix, we define it as a final variable. - If R was declared as final, we
+ * would have to define it in the Rotation constructor (as we have done for
+ * "angles"). However, at the current development stage, we are not sure that
+ * the calculation of R as a function of angles and multiplying them by a given
+ * vector is more efficient that applying a rotation directly to a
+ * multidimensional axis.
+ *
  * @author deanton
  */
 public class Rotation {
 
     private final double[] angles;
-    private int q;
+    private final int q;
     private Matrix R;
 
     public Rotation(double[] angles) {
-        if (angles ==null || angles.length == 0)
+        if (Arrays2.isNullOrEmpty(angles)) {
             throw new MatrixException("Invalid rotation");
+        }
 
-           
         this.angles = angles.clone();
-        
-        q = getq(); 
-        
-        if (angles.length != q*(q-1)/2)
+        this.q = (int) (0.5 * (1 + Math.sqrt(1 + 8 * this.angles.length)));
+
+        if (angles.length != q * (q - 1) / 2) {
             throw new MatrixException("Invalid number of angles");
-      
+        }
     }
 
     public Matrix getRotation() {
@@ -55,26 +64,20 @@ public class Rotation {
         return R;
     }
 
-       public int getq() {
-        if (q == 0) {
-          q = (int) (0.5 * (1 + Math.sqrt(1 + 8 * angles.length)));
-        }
+    public int getQ() {
         return q;
     }
-       
-       public double[]  getangles() {
-       
+
+    public double[] getAngles() {
         return angles;
     }
-       
+
     private void calcR() {
-        q = (int) (0.5 * (1 + Math.sqrt(1 + 8 * angles.length)));
         R = Matrix.identity(q);
         Matrix Ra = new Matrix(2, 2);
         int[] irows = new int[2];
         int[] icols = new int[2];
 
-    
         int pairwise = 0;
 
         double alpha = 0;
@@ -83,7 +86,6 @@ public class Rotation {
             for (int kk = j + 1; kk < q; kk++) {
 
                 alpha = angles[pairwise];
-               
 
                 Ra.subMatrix(0, 1, 0, 1).set(Math.cos(alpha));
                 Ra.subMatrix(1, 2, 0, 1).set(-Math.sin(alpha));
@@ -101,16 +103,12 @@ public class Rotation {
                 R = R.times(temp);
 
                 pairwise = +1;
-
             }
         }
     }
 
     // rendre plus eficace (peut-etre)
     public Matrix apply(Matrix Q) {
-
-        Matrix CR = Q.times(getRotation());
-        return CR;
+        return Q.times(getRotation());
     }
-
 }
