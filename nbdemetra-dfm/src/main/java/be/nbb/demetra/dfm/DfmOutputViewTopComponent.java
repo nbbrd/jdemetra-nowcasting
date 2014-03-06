eaa46b465c7ae95079e8267a1436b9b0effbdd3a
Philippe Charles <philippe.charles@nbb.be>
2014-03-06 16:45:03 +0000

    Improved ui.
    git-svn-id: http://srvdqrd2:8008/svn/jdemetra_seer/trunk@51 6a911cac-6de7-584c-b8a8-48824e541a73

--- nbdemetra-dfm/src/main/java/be/nbb/demetra/dfm/DfmOutputViewTopComponent.java
+++ nbdemetra-dfm/src/main/java/be/nbb/demetra/dfm/DfmOutputViewTopComponent.java
@@ -5,11 +5,35 @@
  */
 package be.nbb.demetra.dfm;
 
-import ec.nbdemetra.ws.WorkspaceFactory;
+import ec.nbdemetra.ui.DemetraUI;
 import ec.nbdemetra.ws.WorkspaceItem;
 import ec.nbdemetra.ws.ui.WorkspaceTopComponent;
 import ec.tss.Dfm.DfmDocument;
+import ec.tss.Dfm.DfmResults;
+import ec.tstoolkit.dfm.DfmInformationSet;
+import ec.tstoolkit.timeseries.simplets.TsData;
+import ec.ui.chart.TsXYDatasets;
+import ec.util.chart.ColorScheme;
+import ec.util.chart.ObsFunction;
+import ec.util.chart.SeriesFunction;
+import ec.util.chart.TimeSeriesChart;
+import ec.util.chart.swing.ColorSchemeIcon;
+import static ec.util.chart.swing.JTimeSeriesChartCommand.applyColorScheme;
+import static ec.util.chart.swing.JTimeSeriesChartCommand.applyLineThickness;
+import ec.util.various.swing.JCommand;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.AbstractAction;
+import javax.swing.AbstractListModel;
+import javax.swing.ComboBoxModel;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import org.netbeans.api.settings.ConvertAsProperties;
 import org.netbeans.core.spi.multiview.CloseOperationState;
@@ -39,6 +63,12 @@ import org.openide.util.NbBundle.Messages;
 })
 public final class DfmOutputViewTopComponent extends WorkspaceTopComponent<DfmDocument> implements MultiViewElement, MultiViewDescription {
 
+    private static final String INITIAL_FACTOR_VISIBLE_PROPERTY = "initialFactorVisible";
+    private static final String NOISE_VISIBLE_PROPERTY = "noiseVisible";
+
+    private boolean initialFactorVisible;
+    private boolean noiseVisible;
+
     public DfmOutputViewTopComponent() {
         this(null);
     }
@@ -48,6 +78,66 @@ public final class DfmOutputViewTopComponent extends WorkspaceTopComponent<DfmDo
         initComponents();
         setName(Bundle.CTL_DfmOutputViewTopComponent());
         setToolTipText(Bundle.HINT_DfmOutputViewTopComponent());
+
+        this.initialFactorVisible = true;
+        this.noiseVisible = true;
+
+        jComboBox1.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                updateChart();
+            }
+        });
+
+        {
+            jTimeSeriesChart1.setSeriesRenderer(new SeriesFunction<TimeSeriesChart.RendererType>() {
+                @Override
+                public TimeSeriesChart.RendererType apply(int series) {
+                    return series == 0 ? TimeSeriesChart.RendererType.LINE : TimeSeriesChart.RendererType.STACKED_COLUMN;
+                }
+            });
+            jTimeSeriesChart1.setSeriesFormatter(new SeriesFunction<String>() {
+                @Override
+                public String apply(int series) {
+                    return jTimeSeriesChart1.getDataset().getSeriesKey(series).toString();
+                }
+            });
+            jTimeSeriesChart1.setObsFormatter(new ObsFunction<String>() {
+                @Override
+                public String apply(int series, int obs) {
+                    return jTimeSeriesChart1.getSeriesFormatter().apply(series) + " - obs " + obs;
+                }
+            });
+
+            JMenu chartMenu = new JMenu();
+            JMenu item;
+            item = new JMenu("Color scheme");
+            for (ColorScheme o : DemetraUI.getInstance().getColorSchemes()) {
+                JMenuItem subItem = item.add(new JCheckBoxMenuItem(applyColorScheme(o).toAction(jTimeSeriesChart1)));
+                subItem.setText(o.getDisplayName());
+                subItem.setIcon(new ColorSchemeIcon(o));
+            }
+            chartMenu.add(item);
+            item = new JMenu("Line thickness");
+            item.add(new JCheckBoxMenuItem(applyLineThickness(1f).toAction(jTimeSeriesChart1))).setText("Thin");
+            item.add(new JCheckBoxMenuItem(applyLineThickness(2f).toAction(jTimeSeriesChart1))).setText("Thick");
+            chartMenu.add(item);
+            jTimeSeriesChart1.setPopupMenu(chartMenu.getPopupMenu());
+        }
+
+        addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                switch (evt.getPropertyName()) {
+                    case INITIAL_FACTOR_VISIBLE_PROPERTY:
+                        updateChart();
+                        break;
+                    case NOISE_VISIBLE_PROPERTY:
+                        updateChart();
+                        break;
+                }
+            }
+        });
     }
 
     /**
@@ -58,19 +148,19 @@ public final class DfmOutputViewTopComponent extends WorkspaceTopComponent<DfmDo
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jTimeSeriesChart1 = new ec.util.chart.swing.JTimeSeriesChart();
+        jComboBox1 = new javax.swing.JComboBox();
+
+        setLayout(new java.awt.BorderLayout());
+        add(jTimeSeriesChart1, java.awt.BorderLayout.CENTER);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        add(jComboBox1, java.awt.BorderLayout.PAGE_START);
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox jComboBox1;
+    private ec.util.chart.swing.JTimeSeriesChart jTimeSeriesChart1;
     // End of variables declaration//GEN-END:variables
     void writeProperties(java.util.Properties p) {
         // better to version settings since initial version as advocated at
@@ -104,9 +194,24 @@ public final class DfmOutputViewTopComponent extends WorkspaceTopComponent<DfmDo
 
     @Override
     public JComponent getToolbarRepresentation() {
-        JToolBar result = new JToolBar();
-        result.addSeparator();
-        return result;
+        JToolBar toolbar = new JToolBar();
+        toolbar.setFloatable(false);
+        toolbar.addSeparator();
+
+        toolbar.add(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateComboBox();
+            }
+        }).setText("UPDATE");
+
+        JToggleButton b1 = (JToggleButton) toolbar.add(new JToggleButton(InitialFactorCommand.INSTANCE.toAction(this)));
+        b1.setText("Show initial factor");
+
+        JToggleButton b2 = (JToggleButton) toolbar.add(new JToggleButton(NoiseCommand.INSTANCE.toAction(this)));
+        b2.setText("Show noise");
+
+        return toolbar;
     }
 
     @Override
@@ -153,6 +258,131 @@ public final class DfmOutputViewTopComponent extends WorkspaceTopComponent<DfmDo
 
     @Override
     protected String getContextPath() {
-        return DfmDocumentManager.CONTEXTPATH; //To change body of generated methods, choose Tools | Templates.
+        return DfmDocumentManager.CONTEXTPATH;
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public boolean isInitialFactorVisible() {
+        return initialFactorVisible;
+    }
+
+    public void setInitialFactorVisible(boolean initialFactorVisible) {
+        boolean old = this.initialFactorVisible;
+        this.initialFactorVisible = initialFactorVisible;
+        firePropertyChange(INITIAL_FACTOR_VISIBLE_PROPERTY, old, this.initialFactorVisible);
+    }
+
+    public boolean isNoiseVisible() {
+        return noiseVisible;
+    }
+
+    public void setNoiseVisible(boolean noiseVisible) {
+        boolean old = this.noiseVisible;
+        this.noiseVisible = noiseVisible;
+        firePropertyChange(NOISE_VISIBLE_PROPERTY, old, this.noiseVisible);
+    }
+    //</editor-fold>
+
+    private void updateComboBox() {
+        DfmResults dfmResult = (DfmResults) getDocument().getElement().getResults().get("dfm");
+
+        jComboBox1.setModel(new InputModel(dfmResult.getInput()));
+        jComboBox1.setSelectedIndex(0);
+    }
+
+    private void updateChart() {
+        DfmResults dfmResult = (DfmResults) getDocument().getElement().getResults().get("dfm");
+
+        int selectedIndex = jComboBox1.getSelectedIndex();
+        TsXYDatasets.Builder b = TsXYDatasets.builder()
+                .add("Actual", dfmResult.getTheData()[selectedIndex]);
+//                .add("Signal", dfmResult.getSignal()[selectedIndex]);
+        TsData[][] x = dfmResult.getShocksDecomposition();
+        for (int i = 0; i < x.length - 2; i++) {
+            b.add("Factor " + i, x[i][selectedIndex]);
+        }
+        if (initialFactorVisible) {
+            b.add("Initial factor", x[x.length - 2][selectedIndex]);
+        }
+        if (noiseVisible) {
+            b.add("Noise", x[x.length - 1][selectedIndex]);
+        }
+
+        jTimeSeriesChart1.setDataset(b.build());
+    }
+
+    private static final class InputModel extends AbstractListModel<TsData> implements ComboBoxModel<TsData> {
+
+        private final DfmInformationSet data;
+        private TsData selectedItem;
+
+        public InputModel(DfmInformationSet data) {
+            this.data = data;
+            this.selectedItem = null;
+        }
+
+        @Override
+        public int getSize() {
+            return data.getSeriesCount();
+        }
+
+        @Override
+        public TsData getElementAt(int index) {
+            return data.series(index);
+        }
+
+        @Override
+        public void setSelectedItem(Object anItem) {
+            if ((selectedItem != null && !selectedItem.equals(anItem))
+                    || selectedItem == null && anItem != null) {
+                selectedItem = (TsData) anItem;
+                fireContentsChanged(this, -1, -1);
+            }
+        }
+
+        @Override
+        public Object getSelectedItem() {
+            return selectedItem;
+        }
+    }
+
+    private static final class InitialFactorCommand extends JCommand<DfmOutputViewTopComponent> {
+
+        public static final InitialFactorCommand INSTANCE = new InitialFactorCommand();
+
+        @Override
+        public void execute(DfmOutputViewTopComponent component) throws Exception {
+            component.setInitialFactorVisible(!component.isInitialFactorVisible());
+        }
+
+        @Override
+        public boolean isSelected(DfmOutputViewTopComponent component) {
+            return component.isInitialFactorVisible();
+        }
+
+        @Override
+        public ActionAdapter toAction(DfmOutputViewTopComponent component) {
+            return super.toAction(component).withWeakPropertyChangeListener(component, INITIAL_FACTOR_VISIBLE_PROPERTY);
+        }
+    }
+
+    private static final class NoiseCommand extends JCommand<DfmOutputViewTopComponent> {
+
+        public static final NoiseCommand INSTANCE = new NoiseCommand();
+
+        @Override
+        public void execute(DfmOutputViewTopComponent component) throws Exception {
+            component.setNoiseVisible(!component.isNoiseVisible());
+        }
+
+        @Override
+        public boolean isSelected(DfmOutputViewTopComponent component) {
+            return component.isNoiseVisible();
+        }
+
+        @Override
+        public ActionAdapter toAction(DfmOutputViewTopComponent component) {
+            return super.toAction(component).withWeakPropertyChangeListener(component, NOISE_VISIBLE_PROPERTY);
+        }
     }
 }
